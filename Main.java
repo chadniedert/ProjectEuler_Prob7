54d39b766ef01c0ffacda72271f01dfd9ba1923b
Chad Niedert <>
2021-04-11 02:20:29 +0000

    works for 6th prime

--- Main.java
+++ Main.java
@@ -1,21 +1,33 @@
 // Link: https://projecteuler.net/problem=7
 
 class Main {
-  public static void main(String[] args) {
-	  
-    
-  }
+	public static void main(String[] args) {
+		int count = 0;
+		int target = 6;
+		int number = 2;
 
-  public boolean isPrime(int num)
-  {
-	  for (int i = 2; i < num; i++)
-	  {
-		  if (num % i == 0 && num != i)
-		  {
-			  return false;
-		  }
-	  }
+		while (count != target)
+		{
+			if (isPrime(number))
+			{
+				count++;
+			}
+			number++;
+		}
 
-	  return true;
-  }
+		System.out.println("The " + target + "th prime is " + (number - 1));
+	}
+
+	public static boolean isPrime(int num)
+	{
+		for (int i = 2; i < num; i++)
+		{
+			if (num % i == 0 && num != i)
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
