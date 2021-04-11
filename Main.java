5dd7ceca9c5642335e7991404138ebadb5320698
Chad Niedert <>
2021-04-11 02:14:30 +0000

    created isPrime method

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+// Link: https://projecteuler.net/problem=7
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+	  
+    
+  }
+
+  public boolean isPrime(int num)
+  {
+	  for (int i = 2; i < num; i++)
+	  {
+		  if (num % i == 0 && num != i)
+		  {
+			  return false;
+		  }
+	  }
+
+	  return true;
   }
 }
